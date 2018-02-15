864d5808104e000e6b99493bd3531ce21f5ba9ed
dusty-nv <dustinf@nvidia.com>
2018-02-15 18:52:12 -0500

    add pi.h

---
+++ pi.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#ifndef __PI_UTILS_H_
+#define __PI_UTILS_H_
+
+
+/**
+ * Constant representing PI
+ */
+#define PI 3.1415926535897932384626433832795f
+
+
+/**
+ * Constant representing two PI
+ */
+#define TWO_PI 6.283185307179586476925286766559f
+
+
+/**
+ * Constant representing PI over two
+ */
+#define PI_OVER_TWO 1.5707963267948966192313216916398f;
+
+
+/**
+ * Constant for converting from degrees to radians
+ */
+#define DEG_TO_RAD 0.017453292519943295769236907684886f
+
+
+/**
+ * Constant for converting from radians to degrees
+ */
+#define RAD_TO_DEG 57.295779513082320876798154814105f
+
+
+#endif
+
