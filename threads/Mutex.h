14e012a536c5372f2b6c6375d08741c25e7928a4
dusty-nv <dustinf@nvidia.com>
2018-05-01 19:53:26 -0400

    add Mutex

---
+++ threads/Mutex.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#include <pthread.h>
+
+
+/**
+ * A lightweight mutual exclusion lock.  It is very fast to check if the mutex is available,
+ * lock it, and release it.  However, if the mutex is unavailable when you attempt to
+ * lock it, execution of the thread will stop until it becomes available.
+ */
+class Mutex
+{
+public:
+	/**
+	 * Constructor
+	 */
+	Mutex();
+
+	/**
+	 * Destructor
+	 */
+	~Mutex();
+
+	/**
+	 * If the lock is free, aquire it.  Otherwise, return without waiting for it to become available.
+	 * @result True if the lock was aquired, false if not.
+	 */
+	inline bool AttemptLock()					{ return (pthread_mutex_trylock(&mID) == 0); }
+	
+	/**
+	 * Aquire the lock, whenever it becomes available.  This could mean just a few instructions
+	 * if the lock is already free, or to block the thread if it isn't.
+	 */
+	inline void Lock()							{ pthread_mutex_lock(&mID); }
+
+	/**
+	 * Release the lock
+	 */
+	inline void Unlock()						{ pthread_mutex_unlock(&mID); }		
+
+	/**
+	 * Wait for the lock, then release it immediately.  Use this in situations where you are waiting for
+	 * an event to occur.
+	 */
+	inline void Sync()							{ Lock(); Unlock(); }
+
+	/**
+	 * Get the mutex object
+	 */
+	pthread_mutex_t* GetID() 					{ return &mID; }
+
+protected:
+	pthread_mutex_t mID;
+};
