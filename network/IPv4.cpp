6e25105a49597e6a53ee556659d3467cef366f7e
dusty-nv <dustinf@nvidia.com>
2018-05-02 12:27:36 -0400

    add IPv4

---
+++ network/IPv4.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#include "IPv4.h"
+
+#include <arpa/inet.h>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <errno.h>
+
+
+// IPv4Address
+bool IPv4Address( const char* str, uint32_t* ipOut )
+{
+	if( !str || !ipOut )
+		return false;
+
+	in_addr addr;
+
+	const int res = inet_pton(AF_INET, str, &addr);
+
+	if( res != 1 )
+	{
+		printf("IPv4Address() - failed to convert '%s' to valid IPv4 address\n", str);
+		return false;
+	}
+	
+	*ipOut = addr.s_addr;
+	return true;
+}
+
+
+// IPv4AddressStr
+std::string IPv4AddressStr( uint32_t ipAddress )
+{
+	char str[INET_ADDRSTRLEN];
+	memset(str, 0, INET_ADDRSTRLEN);
+
+	if( inet_ntop(AF_INET, &ipAddress, str, INET_ADDRSTRLEN) == NULL )
+		printf("IPv4AddressStr() - failed to convert 0x%08X to string\n", ipAddress);
+	
+	return std::string(str);
+}
---
+++ network/IPv4.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#ifndef __NETWORK_IPV4_H_
+#define __NETWORK_IPV4_H_
+
+#include <string>
+
+
+/**
+ * Convert an IPv4 address string in "xxx.xxx.xxx.xxx" format to binary representation.
+ * @param str the IPv4 string, in "xxx.xxx.xxx.xxx" format
+ * @param ip_out output pointer to converted IPv4 address, in network byte order.
+ * @returns true, if str was a valid IPv4 address and the conversion was successful.
+ *          false, if the conversion failed.
+ */
+bool IPv4Address( const char* str, uint32_t* ip_out );
+
+
+/**
+ * Return text string of IPv4 address in "xxx.xxx.xxx.xxx" format
+ * @param ip_address IPv4 address, supplied in network byte order.
+ */
+std::string IPv4AddressStr( uint32_t ip_address );
+
+
+#endif
