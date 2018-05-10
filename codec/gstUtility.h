ffbde673c2b238bdc328a0c1c69351e6324215c1
dusty-nv <dustinf@nvidia.com>
2018-05-10 19:36:50 -0400

    update gstUtility

--- codec/gstUtility.h
+++ codec/gstUtility.h
@@ -23,27 +23,34 @@
 #ifndef __GSTREAMER_UTILITY_H__
 #define __GSTREAMER_UTILITY_H__
 
-
 #include <gst/gst.h>
+#include <string>
+
+
+/**
+ * gstCodec enumeration
+ */
+enum gstCodec
+{
+	GST_CODEC_H264 = 0,
+	GST_CODEC_H265
+};
 
 
 /**
  * LOG_GSTREAMER printf prefix
- * @ingroup util
  */
 #define LOG_GSTREAMER "[gstreamer] "
 
 
 /**
  * gstreamerInit
- * @ingroup util
  */
 bool gstreamerInit();
 
 
 /**
  * gst_message_print
- * @ingroup util
  */
 gboolean gst_message_print(_GstBus* bus, _GstMessage* message, void* user_data);
 
