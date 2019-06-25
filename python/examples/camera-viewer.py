c94a424331cfc805ab85739819a01821999698c1
dusty-nv <dustinf@nvidia.com>
2019-06-25 11:10:26 -0400

    fixed camera sample

--- python/examples/camera-viewer.py
+++ python/examples/camera-viewer.py
@@ -30,13 +30,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--width", type=int, default=1280, help="desired width of camera stream (default is 1280 pixels)")
 parser.add_argument("--height", type=int, default=720, help="desired height of camera stream (default is 720 pixels)")
-parser.add_argument("--camera", type=string, default=None, help="index of the MIPI CSI camera to use (NULL for CSI camera 0), or for VL42 cameras the /dev/video node to use (e.g. /dev/video0).  By default, MIPI CSI camera 0 will be used.")
+parser.add_argument("--camera", type=str, default="0", help="index of the MIPI CSI camera to use (NULL for CSI camera 0), or for VL42 cameras the /dev/video node to use (e.g. /dev/video0).  By default, MIPI CSI camera 0 will be used.")
 
 opt = parser.parse_args()
 print(opt)
 
 # create display window
-display = jetson.utils.glDisplay("Camera Viewer")
+display = jetson.utils.glDisplay()
 
 # create camera device
 camera = jetson.utils.gstCamera(opt.width, opt.height, opt.camera)
@@ -48,7 +48,7 @@ camera.Open()
 while display.IsOpen():
 	image, width, height = camera.CaptureRGBA()
 	display.RenderOnce(image, width, height)
-	display.SetTitle("{:s} | {:d}x{:d} | {:.0f} FPS".format(opt.title, width, height, display.GetFPS()))
+	display.SetTitle("{:s} | {:d}x{:d} | {:.0f} FPS".format("Camera Viewer", width, height, display.GetFPS()))
 	
 # close the camera
 camera.Close()
