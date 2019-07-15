af6e9c2c79fc8075b745e1add5c86df1e5f2cc97
nano <msw@becod.co.kr>
2019-07-15 14:41:50 +0900

    add resize feature but slow, commented

--- camera/gstCamera.cpp
+++ camera/gstCamera.cpp
@@ -441,11 +441,10 @@ bool gstCamera::buildLaunchStr( gstCameraSrc src )
 	else if ( src == GST_SOURCE_RTSP )
 	{
 		ss << "rtspsrc location=" << mCameraStr << " latency=0 ! ";
-		//ss << "queue ! rtph264depay ! h264parse ! queue ! omxh264dec ! videoconvert ! video/x-raw, format=RGB ! videoconvert ! ";
-		ss << "decodebin ! ";
-        ss << "nvvidconv ! ";
-        ss << "video/x-raw, format=I420, width=" << mWidth << ", height=" << mHeight << " ! ";
-		//videoscale ! video/x-raw,width=1280,height=800
+		ss << "queue ! rtph264depay ! h264parse ! queue ! omxh264dec ! ";
+		ss << "videoconvert ! video/x-raw, format=RGB ! ";
+		//ss << "videoconvert ! videoscale ! video/x-raw, format=RGB, width=" << mWidth << ", height=" << mHeight << " ! ";
+		
 		ss << "appsink name=mysink";
 	}
 	
