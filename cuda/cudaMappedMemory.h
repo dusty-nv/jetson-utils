4bbf247562f566888ca467c4a3d6ba732e14180d
dusty-nv <dustinf@nvidia.com>
2019-08-19 13:52:00 -0400

    added cudaAllocMapped() overload

--- cuda/cudaMappedMemory.h
+++ cuda/cudaMappedMemory.h
@@ -29,6 +29,12 @@
 
 /**
  * Allocate ZeroCopy mapped memory, shared between CUDA and CPU.
+ * @note although two pointers are returned, one for CPU and GPU,
+ *       they both resolve to the same physical memory.
+ * @param[out] cpuPtr Returned CPU pointer to the shared memory.
+ * @param[out] gpuPtr Returned GPU pointer to the shared memory.
+ * @param[in] size Size (in bytes) of the shared memory to allocate.
+ * @returns `true` if the allocation succeeded, `false` otherwise.
  * @ingroup cuda
  */
 inline bool cudaAllocMapped( void** cpuPtr, void** gpuPtr, size_t size )
@@ -46,6 +52,38 @@ inline bool cudaAllocMapped( void** cpuPtr, void** gpuPtr, size_t size )
 
 	memset(*cpuPtr, 0, size);
 	//printf(LOG_CUDA "cudaAllocMapped %zu bytes, CPU %p GPU %p\n", size, *cpuPtr, *gpuPtr);
+	return true;
+}
+
+
+/**
+ * Allocate ZeroCopy mapped memory, shared between CUDA and CPU.
+ * @note this overload of cudaAllocMapped returns one pointer,
+ *       assumes that the CPU and GPU addresses will match  
+ *       (as is the case with any recent CUDA version).
+ * @param[out] ptr Returned pointer to the shared CPU/GPU memory.
+ * @param[in] size Size (in bytes) of the shared memory to allocate.
+ * @returns `true` if the allocation succeeded, `false` otherwise.
+ * @ingroup cuda
+ */
+inline bool cudaAllocMapped( void** ptr, size_t size )
+{
+	void* cpuPtr = NULL;
+	void* gpuPtr = NULL;
+
+	if( !ptr || size == 0 )
+		return false;
+
+	if( !cudaAllocMapped(&cpuPtr, &gpuPtr, size) )
+		return false;
+
+	if( cpuPtr != gpuPtr )
+	{
+		printf(LOG_CUDA "cudaAllocMapped() - addresses of CPU and GPU pointers don't match\n");
+		return false;
+	}
+
+	*ptr = gpuPtr;
 	return true;
 }
 
