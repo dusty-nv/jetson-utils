a3bdb632b5b948e1f35ecee4b7ae4fb0a343a506
dusty-nv <dustinf@nvidia.com>
2019-11-25 15:18:35 -0500

    glTexture updates

--- display/glTexture.cpp
+++ display/glTexture.cpp
@@ -26,6 +26,10 @@
 #include "cudaMappedMemory.h"
 
 
+// from glBuffer.cpp
+cudaGraphicsRegisterFlags cudaGraphicsRegisterFlagsFromGL( uint32_t flags );
+
+
 //-----------------------------------------------------------------------------------
 inline uint32_t glTextureLayout( uint32_t format )
 {
@@ -165,25 +169,47 @@ inline uint glTextureTypeSize( uint32_t format )
 glTexture::glTexture()
 {
 	mID     = 0;
-	mDMA    = 0;
 	mWidth  = 0;
 	mHeight = 0;
 	mFormat = 0;
 	mSize   = 0;
-	
-	mInteropCUDA   = NULL;
-	mInteropHost   = NULL;
-	mInteropDevice = NULL;
+
+	mPackDMA   = 0;
+	mUnpackDMA = 0;
+
+	mMapDevice = 0;
+	mMapFlags  = 0;
+
+	mInteropPack   = NULL;
+	mInteropUnpack = NULL;
 }
 
 
 // destructor
 glTexture::~glTexture()
 {
-	if( mInteropCUDA != NULL )
+	if( mInteropPack != NULL )
 	{
-		CUDA(cudaGraphicsUnregisterResource(mInteropCUDA));
-		mInteropCUDA = NULL;
+		CUDA(cudaGraphicsUnregisterResource(mInteropPack));
+		mInteropPack = NULL;
+	}
+
+	if( mInteropUnpack != NULL )
+	{
+		CUDA(cudaGraphicsUnregisterResource(mInteropUnpack));
+		mInteropUnpack = NULL;
+	}
+
+	if( mPackDMA != 0 )
+	{
+		GL(glDeleteBuffers(1, &mPackDMA));
+		mPackDMA = 0;
+	}
+
+	if( mUnpackDMA != 0 )
+	{
+		GL(glDeleteBuffers(1, &mUnpackDMA));
+		mUnpackDMA = 0;
 	}
 
 	if( mID != 0 )
@@ -225,6 +251,7 @@ bool glTexture::init( uint32_t width, uint32_t height, uint32_t format, void* da
 	GL(glBindTexture(GL_TEXTURE_2D, id));
 	
 	// set default texture parameters
+	GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0));
 	GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
 	GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 	GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
@@ -233,28 +260,358 @@ bool glTexture::init( uint32_t width, uint32_t height, uint32_t format, void* da
 	printf(LOG_GL "creating %ux%u texture\n", width, height);
 	
 	// allocate texture
-	GL_VERIFYN(glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, glTextureLayout(format), glTextureType(format), data));
-	GL(glBindTexture(GL_TEXTURE_2D, 0));
-	
-	// allocate DMA PBO
-	uint32_t dma = 0;
-	
-	GL(glGenBuffers(1, &dma));
-	GL(glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, dma));
-	GL(glBufferDataARB(GL_PIXEL_UNPACK_BUFFER_ARB, size, NULL, GL_DYNAMIC_DRAW_ARB));
-	GL(glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, 0));
+	GL_VERIFY(glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, glTextureLayout(format), glTextureType(format), data));
 	
 	mID     = id;
-	mDMA    = dma;
 	mWidth  = width;
 	mHeight = height;
 	mFormat = format;
 	mSize   = size;
 
+	GL(glBindTexture(GL_TEXTURE_2D, 0));
+	GL(glDisable(GL_TEXTURE_2D));
+
 	return true;
 }
 
 
+// allocDMA
+uint32_t glTexture::allocDMA( uint32_t type )
+{
+	if( type == GL_PIXEL_PACK_BUFFER_ARB && mPackDMA != 0 )
+		return mPackDMA;
+	else if( type == GL_PIXEL_UNPACK_BUFFER_ARB && mUnpackDMA != 0 )
+		return mUnpackDMA;
+	
+	// allocate PBO
+	uint32_t dma = 0;
+	
+	GL_VERIFY(glGenBuffers(1, &dma));
+	GL_VERIFY(glBindBufferARB(type, dma));
+	GL_VERIFY(glBufferDataARB(type, mSize, NULL, GL_DYNAMIC_DRAW_ARB));
+	GL_VERIFY(glBindBufferARB(type, 0));
+
+	if( type == GL_PIXEL_PACK_BUFFER_ARB )
+		mPackDMA = dma;
+	else if( type == GL_PIXEL_UNPACK_BUFFER_ARB )
+		mUnpackDMA = dma;
+
+	return dma;
+}
+	
+
+// allocInterop
+cudaGraphicsResource* glTexture::allocInterop( uint32_t type, uint32_t flags )
+{
+	if( type == GL_PIXEL_PACK_BUFFER_ARB && mInteropPack != NULL )
+		return mInteropPack;
+	else if( type == GL_PIXEL_UNPACK_BUFFER_ARB && mInteropUnpack != NULL )
+		return mInteropUnpack;
+
+	cudaGraphicsResource* interop = NULL;
+
+	if( CUDA_FAILED(cudaGraphicsGLRegisterBuffer(&interop, allocDMA(type), cudaGraphicsRegisterFlagsFromGL(flags))) )
+		return NULL;
+
+	if( type == GL_PIXEL_PACK_BUFFER_ARB )
+		mInteropPack = interop;
+	else if( type == GL_PIXEL_UNPACK_BUFFER_ARB )
+		mInteropUnpack = interop;
+
+	printf(LOG_CUDA "registered openGL texture for interop access (%ux%u, %u bytes)\n", mWidth, mHeight, mSize);
+	return interop;
+}
+
+
+// Bind
+bool glTexture::Bind()
+{
+	if( !mID )
+		return false;
+
+	GL_VERIFY(glEnable(GL_TEXTURE_2D));
+	GL_VERIFY(glActiveTextureARB(GL_TEXTURE0_ARB));
+	GL_VERIFY(glBindTexture(GL_TEXTURE_2D, mID));
+
+	return true;
+}
+
+
+// Unbind
+void glTexture::Unbind()
+{
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_TEXTURE_2D);
+}
+
+
+// Render
+void glTexture::Render( const float4& rect )
+{
+	if( !Bind() )
+		return;
+
+	glBegin(GL_QUADS);
+
+		glColor4f(1.0f,1.0f,1.0f,1.0f);
+
+		glTexCoord2f(0.0f, 0.0f); 
+		glVertex2f(rect.x, rect.y);
+
+		glTexCoord2f(1.0f, 0.0f); 
+		glVertex2f(rect.z, rect.y);	
+
+		glTexCoord2f(1.0f, 1.0f); 
+		glVertex2f(rect.z, rect.w);
+
+		glTexCoord2f(0.0f, 1.0f); 
+		glVertex2f(rect.x, rect.w);
+
+	glEnd();
+	Unbind();
+}
+
+
+// Render
+void glTexture::Render( float x, float y )
+{
+	Render(x, y, mWidth, mHeight);
+}
+
+
+// Render
+void glTexture::Render( float x, float y, float width, float height )
+{
+	Render(make_float4(x, y, x + width, y + height));
+}
+
+
+
+// dmaTypeFromFlags
+static inline uint32_t dmaTypeFromFlags( uint32_t flags )
+{
+	if( flags == GL_READ_ONLY )
+		return GL_PIXEL_PACK_BUFFER_ARB;
+	else
+		return GL_PIXEL_UNPACK_BUFFER_ARB;	// GL_READ_WRITE?
+}
+
+
+// Map
+void* glTexture::Map( uint32_t device, uint32_t flags )
+{
+	if( mMapDevice != 0 )
+	{
+		printf(LOG_GL "error -- glTexture is already mapped (call Unmap() first)\n");
+		return NULL;
+	}
+
+	if( device != GL_MAP_CPU && device != GL_MAP_CUDA )
+	{
+		printf(LOG_GL "glTexture::Map() -- invalid device (must be GL_MAP_CPU or GL_MAP_CUDA)\n");
+		return NULL;
+	}
+
+	if( !Bind() )
+		return NULL;
+
+	// make sure DMA buffer is allocated	
+	void* dmaPtr = NULL;
+
+	const uint32_t dmaType = dmaTypeFromFlags(flags);
+	const uint32_t dmaBuffer = allocDMA(dmaType);
+
+	if( !dmaBuffer )
+		return NULL;
+
+	if( device == GL_MAP_CPU )
+	{
+		GL(glBindBuffer(dmaType, dmaBuffer));
+
+		if( flags == GL_WRITE_DISCARD )
+		{
+			// invalidate the old buffer so we can map without stalling
+			GL(glBufferDataARB(GL_PIXEL_UNPACK_BUFFER_ARB, mSize, NULL, GL_STREAM_DRAW_ARB));	
+			flags = GL_WRITE_ONLY; // GL expects GL_WRITE_ONLY
+		}
+		else if( flags == GL_READ_ONLY )
+		{
+			// read data from texture to PBO
+			GL(glReadPixels(0, 0, mWidth, mHeight, glTextureLayout(mFormat), glTextureType(mFormat), NULL));
+		}
+
+		// lock the PBO buffer
+		dmaPtr = glMapBuffer(dmaType, flags);
+
+		if( !dmaPtr )
+		{
+			printf(LOG_GL "glMapBuffer() failed\n");
+			GL_CHECK("glMapBuffer()\n");
+			return NULL;
+		}
+	}
+	else if( device == GL_MAP_CUDA )
+	{
+		if( flags == GL_READ_ONLY )
+		{
+			GL(glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, mPackDMA));
+			GL(glReadPixels(0, 0, mWidth, mHeight, glTextureLayout(mFormat), glTextureType(mFormat), NULL));
+			GL(glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, 0));
+		}
+
+		// make sure CUDA resource is registered
+		cudaGraphicsResource* interop = allocInterop(dmaType, flags);
+
+		if( !interop )
+			return NULL;
+
+		if( mMapFlags != 0 && mMapFlags != flags )	// TODO two buffers, but one set of flags
+			CUDA(cudaGraphicsResourceSetMapFlags(interop, cudaGraphicsRegisterFlagsFromGL(flags)));
+
+		if( CUDA_FAILED(cudaGraphicsMapResources(1, &interop)) )
+			return NULL;
+
+		// map CUDA device pointer
+		size_t mappedSize = 0;
+
+		if( CUDA_FAILED(cudaGraphicsResourceGetMappedPointer(&dmaPtr, &mappedSize, interop)) )
+		{
+			CUDA(cudaGraphicsUnmapResources(1, &interop));
+			return NULL;
+		}
+		
+		if( mSize != mappedSize )
+			printf(LOG_GL "glTexture::Map() -- CUDA size mismatch %zu bytes  (expected=%u)\n", mappedSize, mSize);
+	}
+
+	mMapDevice = device;
+	mMapFlags  = flags;
+
+	return dmaPtr;
+}
+
+
+// Unmap
+void glTexture::Unmap()
+{
+	if( mMapDevice != GL_MAP_CPU && mMapDevice != GL_MAP_CUDA )
+		return;
+
+	if( !Bind() )
+		return;
+
+	const uint32_t dmaType = dmaTypeFromFlags(mMapFlags);
+
+	if( mMapDevice == GL_MAP_CPU )
+	{
+		GL(glUnmapBuffer(dmaType));
+
+		if( mMapFlags != GL_READ_ONLY )
+			GL(glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, mWidth, mHeight, glTextureLayout(mFormat), glTextureType(mFormat), NULL));
+
+		GL(glBindBuffer(dmaType, 0));
+	}
+	else if( mMapDevice == GL_MAP_CUDA )
+	{
+		cudaGraphicsResource* interop = allocInterop(dmaType, mMapFlags);
+
+		if( !interop )
+			return;
+
+		CUDA(cudaGraphicsUnmapResources(1, &interop));
+
+		if( mMapFlags != GL_READ_ONLY )
+		{
+			GL(glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, mUnpackDMA));
+			GL(glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, mWidth, mHeight, glTextureLayout(mFormat), glTextureType(mFormat), NULL));
+			GL(glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, 0));	
+		}
+	}
+
+	mMapDevice = 0;
+	Unbind();
+}
+
+
+// Copy
+bool glTexture::Copy( void* ptr, uint32_t offset, uint32_t size, uint32_t flags )
+{
+	if( !ptr || size == 0 || size >= mSize || offset >= mSize || offset > (mSize - size) )
+		return false;
+
+	uint32_t mapFlags = GL_READ_ONLY;
+
+	if( flags == GL_FROM_CPU || flags == GL_FROM_CUDA )
+	{
+		if( size == mSize )
+			mapFlags = GL_WRITE_DISCARD;
+		else
+			mapFlags = GL_WRITE_ONLY;
+	}
+	
+	if( flags == GL_FROM_CPU )
+	{
+		// TODO for faster CPU path, see http://hacksoflife.blogspot.com/2015/06/glmapbuffer-no-longer-cool.html
+		void* dst = Map(GL_MAP_CPU, mapFlags);
+
+		if( !dst )
+			return false;
+
+		memcpy((uint8_t*)dst + offset, ptr, size);
+	}
+	else if( flags == GL_FROM_CUDA )
+	{
+		void* dst = Map(GL_MAP_CUDA, mapFlags);
+
+		if( !dst )
+			return false;
+
+		if( CUDA_FAILED(cudaMemcpy((uint8_t*)dst + offset, ptr, size, cudaMemcpyDeviceToDevice)) )
+		{
+			Unmap();
+			return false;
+		}
+	}
+	else if( flags == GL_TO_CPU )
+	{
+		void* src = Map(GL_MAP_CPU, mapFlags);
+
+		if( !src )
+			return false;
+	
+		memcpy(ptr, (uint8_t*)src + offset, size);
+	}
+	else if( flags == GL_TO_CUDA )
+	{
+		void* src = Map(GL_MAP_CUDA, mapFlags);
+
+		if( !src )
+			return false;
+	
+		if( CUDA_FAILED(cudaMemcpy(ptr, (uint8_t*)src + offset, size, cudaMemcpyDeviceToDevice)) )
+		{
+			Unmap();
+			return false;
+		}
+	}
+
+	Unmap();
+	return true;
+}
+
+// Copy
+bool glTexture::Copy( void* ptr, uint32_t size, uint32_t flags )
+{
+	return Copy(ptr, 0, mSize, flags);
+}
+
+// Copy
+bool glTexture::Copy( void* ptr, uint32_t flags )
+{
+	return Copy(ptr, 0, mSize, flags);
+}
+
+#if 0
 // MapCUDA
 void* glTexture::MapCUDA()
 {
@@ -361,46 +718,6 @@ bool glTexture::UploadCPU( void* data )
 
 	return true;
 }
-
-	
-// Render
-void glTexture::Render( const float4& rect )
-{
-	GL(glEnable(GL_TEXTURE_2D));
-	GL(glBindTexture(GL_TEXTURE_2D, mID));
-
-	glBegin(GL_QUADS);
-
-		glColor4f(1.0f,1.0f,1.0f,1.0f);
-
-		glTexCoord2f(0.0f, 0.0f); 
-		glVertex2f(rect.x, rect.y);
-
-		glTexCoord2f(1.0f, 0.0f); 
-		glVertex2f(rect.z, rect.y);	
-
-		glTexCoord2f(1.0f, 1.0f); 
-		glVertex2f(rect.z, rect.w);
-
-		glTexCoord2f(0.0f, 1.0f); 
-		glVertex2f(rect.x, rect.w);
-
-	glEnd();
-
-	GL(glBindTexture(GL_TEXTURE_2D, 0));
-	GL(glDisable(GL_TEXTURE_2D));
-}
-
-
-
-void glTexture::Render( float x, float y )
-{
-	Render(x, y, mWidth, mHeight);
-}
-
-void glTexture::Render( float x, float y, float width, float height )
-{
-	Render(make_float4(x, y, x + width, y + height));
-}
+#endif
 
 
--- display/glTexture.h
+++ display/glTexture.h
@@ -66,6 +66,16 @@ public:
 	~glTexture();
 	
 	/**
+	 * Activate using the texture
+	 */
+	bool Bind();
+
+	/**
+	 * Deactivate using the texture
+	 */
+	void Unbind();
+
+	/**
 	 * Render the texture at the specified window coordinates.
 	 */
 	void Render( float x, float y );
@@ -106,36 +116,110 @@ public:
 	inline uint32_t GetSize() const	{ return mSize; }
 	
 	/**
-	 * Map the texture for access in CUDA
+	 * Map the texture for accessing from the CPU or CUDA.
+	 *
+	 * @param device either GL_MAP_CPU or GL_MAP_CUDA
+	 *
+	 * @param flags should be one of the following:
+	 *                 - GL_READ_WRITE
+	 *                 - GL_READ_ONLY
+	 *                 - GL_WRITE_ONLY
+	 *                 - GL_WRITE_DISCARD
+	 *
+	 * @returns CPU pointer to buffer if GL_MAP_CPU was specified,
+	 *          CUDA device pointer to buffer if GL_MAP_CUDA was specified,
+	 *          or NULL if an error occurred mapping the buffer.                  
 	 */
-	void* MapCUDA();
+	void* Map( uint32_t device, uint32_t flags );
 
 	/**
-	 * Unmap the texture from CUDA
+	 * Unmap the texture from CPU/CUDA access.
+	 * @note the texture will be unbound after calling Unmap()
 	 */
 	void Unmap();
-	
+
 	/**
-	 * Update the contents of the texture from a CPU pointer.
+	 * Copy entire contents of the texture to/from CPU or CUDA memory.
+	 *
+	 * @param ptr the memory pointer to copy to/from, either in
+	 *            CPU or CUDA address space depending on flags.
+	 *            It's assumed that the size of the memory from
+	 *            this pointer is equal to GetSize(), and the
+	 *            entire contents of the buffer will be copied.
+	 *
+	 * @param flags should be one of the following:
+	 *           - GL_FROM_CPU  (copy from CPU->OpenGL)
+	 *           - GL_FROM_CUDA (copy from CUDA->OpenGL)
+	 *           - GL_TO_CPU    (copy from OpenGL->CPU)
+	 *           - GL_TO_CUDA   (copy from OpenGL->CUDA)
+	 *
+	 * @returns true on success, false on failure
 	 */
-	bool UploadCPU( void* data );
+	bool Copy( void* ptr, uint32_t flags );
+
+	/**
+	 * Copy contents of the texture to/from CPU or CUDA memory.
+	 *
+	 * @param ptr the memory pointer to copy to/from, either in
+	 *            CPU or CUDA address space depending on flags.
+	 *
+	 * @param size the number of bytes to copy
+	 * 
+	 * @param flags should be one of the following:
+	 *           - GL_FROM_CPU  (copy from CPU->OpenGL)
+	 *           - GL_FROM_CUDA (copy from CUDA->OpenGL)
+	 *           - GL_TO_CPU    (copy from OpenGL->CPU)
+	 *           - GL_TO_CUDA   (copy from OpenGL->CUDA)
+	 *
+	 * @returns true on success, false on failure
+	 */
+	bool Copy( void* ptr, uint32_t size, uint32_t flags );
+
+	/**
+	 * Copy contents of the texture to/from CPU or CUDA memory.
+	 *
+	 * @param ptr the memory buffer to copy to/from, either in
+	 *            CPU or CUDA address space depending on flags
+	 *
+	 * @param offset the offset into the OpenGL buffer to copy.
+	 *               It is assumed any offset to the CPU/CUDA 
+	 *               pointer argument has already been applied.
+	 *
+	 * @param size the number of bytes to copy
+	 * 
+	 * @param flags should be one of the following:
+	 *           - GL_FROM_CPU  (copy from CPU->OpenGL)
+	 *           - GL_FROM_CUDA (copy from CUDA->OpenGL)
+	 *           - GL_TO_CPU    (copy from OpenGL->CPU)
+	 *           - GL_TO_CUDA   (copy from OpenGL->CUDA)
+	 *
+	 * @returns true on success, false on failure
+	 */
+	bool Copy( void* ptr, uint32_t offset, uint32_t size, uint32_t flags );
+
 	
 private:
 	glTexture();
 
 	bool init( uint32_t width, uint32_t height, uint32_t format, void* data );
-	
+
+	uint32_t allocDMA( uint32_t type );
+	cudaGraphicsResource* allocInterop( uint32_t type, uint32_t flags );
+
 	uint32_t mID;
-	uint32_t mDMA;
+	uint32_t mPackDMA;
+	uint32_t mUnpackDMA;
+
 	uint32_t mWidth;
 	uint32_t mHeight;
 	uint32_t mFormat;
 	uint32_t mSize;
-	
-	cudaGraphicsResource* mInteropCUDA;
 
-	void* mInteropHost;
-	void* mInteropDevice;
+	uint32_t mMapDevice;
+	uint32_t mMapFlags;
+
+	cudaGraphicsResource* mInteropPack;
+	cudaGraphicsResource* mInteropUnpack;
 };
 
 
