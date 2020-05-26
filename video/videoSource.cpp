0935912db4ccb89534325ed68fd0631757f786aa
Dustin Franklin <dustinf@nvidia.com>
2020-05-26 17:03:20 -0400

    added new videoSource API

---
+++ video/videoSource.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#include "videoSource.h"
+
+
+// constructor
+videoSource::videoSource( const videoOptions& options ) : mOptions(options)
+{
+	mStreaming = false;
+}
+
+
+// destructor
+videoSource::~videoSource()
+{
+
+}
+
+
+// Create
+videoSource* videoSource::Create( const videoOptions& options )
+{
+	return NULL;
+}
+
+
+// Create
+videoSource* videoSource::Create( const char* device, const videoOptions& options )
+{
+	videoOptions opt = options;
+	opt.device = device;
+	return Create(opt);
+}
+
+
+// Create
+videoSource* videoSource::Create( const int argc, char** argv )
+{
+	if( argc < 0 || !argv )
+		return NULL;
+
+	commandLine cmdLine(argc, argv);
+	return Create(cmdLine);
+}
+
+
+// Create
+videoSource* videoSource::Create( const commandLine& cmdLine )
+{
+	videoOptions opt;
+
+	// TODO parse videoOptions
+
+	return Create(opt);
+}
+
+
+// Open
+bool videoSource::Open()
+{
+	mStreaming = true;
+	return true;
+}
+
+
+// Close
+void videoSource::Close()
+{
+	mStreaming = false;
+}
+
+
---
+++ video/videoSource.h
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#ifndef __VIDEO_SOURCE_H_
+#define __VIDEO_SOURCE_H_
+
+
+#include "videoOptions.h"
+#include "imageFormat.h"		
+#include "commandLine.h"
+
+
+/**
+ * @ingroup image
+ */
+class videoSource
+{
+public:
+	/**
+	 *
+	 */
+	static videoSource* Create( const videoOptions& options );
+
+	/**
+	 *
+	 */
+	static videoSource* Create( const char* device, const videoOptions& options=videoOptions() );
+
+	/**
+	 *
+	 */
+	static videoSource* Create( const int argc, char** argv );
+
+	/**
+	 *
+	 */
+	static videoSource* Create( const commandLine& cmdLine );
+	
+	/**
+	 *
+	 */
+	virtual ~videoSource();
+
+	/**
+	 *
+	 */
+	virtual bool Capture( void** image, imageFormat format, uint64_t timeout=UINT64_MAX ) = 0;
+
+	/**
+	 *
+	 */
+	template<typename T> bool Capture( T** image, uint64_t timeout=UINT64_MAX )		{ return Capture((void**)image, imageFormatFromType<T>(), timeout); }
+	
+	//inline bool Capture( uchar3** image, uint64_t timeout=UINT64_MAX )		{ return Capture((void**)image, FORMAT_RGB8, timeout); }
+
+	/**
+	 *
+	 */
+	//inline bool Capture( uchar4** image, uint64_t timeout=UINT64_MAX );		{ return Capture((void**)image, FORMAT_RGBA8, timeout); }
+
+	/**
+	 *
+	 */
+	//inline bool Capture( float3** image, uint64_t timeout=UINT64_MAX );		{ return Capture((void**)image, FORMAT_RGB32, timeout); }
+
+	/**
+	 *
+	 */
+	//inline bool Capture( float4** image, uint64_t timeout=UINT64_MAX );		{ return Capture((void**)image, FORMAT_RGBA32, timeout); }
+
+	/**
+	 * Begin streaming the camera.
+	 * After Open() is called, frames from the camera will begin to be captured.
+	 *
+	 * Open() is not stricly necessary to call, if you call one of the Capture()
+	 * functions they will first check to make sure that the stream is opened,
+	 * and if not they will open it automatically for you.
+	 *
+	 * @returns `true` on success, `false` if an error occurred opening the stream.
+	 */
+	virtual bool Open();
+
+	/**
+	 * Stop streaming the camera.
+	 * @note Close() is automatically called by the camera's destructor when
+	 * it gets deleted, so you do not explicitly need to call Close() before
+	 * exiting the program if you delete your camera object.
+	 */
+	virtual void Close();
+
+	/**
+	 * Check if the camera is streaming or not.
+	 * @returns `true` if the camera is streaming (open), or `false` if it's closed.
+	 */
+	inline bool IsStreaming() const	   			{ return mStreaming; }
+
+	/**
+	 *
+	 */
+	inline uint32_t GetWidth() const				{ return mOptions.width; }
+
+	/**
+	 *
+	 */
+	inline uint32_t GetHeight() const				{ return mOptions.height; }
+	
+	/**
+	 *
+	 */
+	inline const char* GetDevice() const			{ return mOptions.device.c_str(); }
+
+	/**
+	 *
+	 */
+	inline const videoOptions& GetOptions() const	{ return mOptions; }
+
+protected:
+	//videoSource();
+	videoSource( const videoOptions& options );
+
+	bool         mStreaming;
+	videoOptions mOptions;
+};
+
+#endif
