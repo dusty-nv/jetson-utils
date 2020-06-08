173e9e5989e79dffcd948d144a207ae6f4463f0c
Dustin Franklin <dustinf@nvidia.com>
2020-06-08 17:06:49 -0400

    added cast_vec<T> operator

--- cuda/cudaVector.h
+++ cuda/cudaVector.h
@@ -52,24 +52,52 @@ template<typename T> struct cuda_assert_false : std::false_type { };
 /**
  * @ingroup cuda
  */
-template<typename T> inline __device__ T make_vec( typename cudaVectorTypeInfo<T>::Base x, typename cudaVectorTypeInfo<T>::Base y, typename cudaVectorTypeInfo<T>::Base z, typename cudaVectorTypeInfo<T>::Base w )	{ static_assert(cuda_assert_false<T>::value, "invalid image format type - supported types are uchar3, uchar4, float3, float4");  }
+template<typename T> inline __host__ __device__ T make_vec( typename cudaVectorTypeInfo<T>::Base x, typename cudaVectorTypeInfo<T>::Base y, typename cudaVectorTypeInfo<T>::Base z, typename cudaVectorTypeInfo<T>::Base w )	{ static_assert(cuda_assert_false<T>::value, "invalid vector type - supported types are uchar3, uchar4, float3, float4");  }
 
-template<> inline __device__ uchar3 make_vec( uint8_t x, uint8_t y, uint8_t z, uint8_t w )	{ return make_uchar3(x,y,z); }
-template<> inline __device__ uchar4 make_vec( uint8_t x, uint8_t y, uint8_t z, uint8_t w )	{ return make_uchar4(x,y,z,w); }
+template<> inline __host__ __device__ uchar3 make_vec( uint8_t x, uint8_t y, uint8_t z, uint8_t w )	{ return make_uchar3(x,y,z); }
+template<> inline __host__ __device__ uchar4 make_vec( uint8_t x, uint8_t y, uint8_t z, uint8_t w )	{ return make_uchar4(x,y,z,w); }
 
-template<> inline __device__ float3 make_vec( float x, float y, float z, float w )			{ return make_float3(x,y,z); }
-template<> inline __device__ float4 make_vec( float x, float y, float z, float w )			{ return make_float4(x,y,z,w); }
+template<> inline __host__ __device__ float3 make_vec( float x, float y, float z, float w )		{ return make_float3(x,y,z); }
+template<> inline __host__ __device__ float4 make_vec( float x, float y, float z, float w )		{ return make_float4(x,y,z,w); }
 
 /**
  * @ingroup cuda
  */
-template<typename T> inline __device__ typename cudaVectorTypeInfo<T>::Base alpha( T vec, typename cudaVectorTypeInfo<T>::Base default_alpha=255 )	{ static_assert(cuda_assert_false<T>::value, "invalid image format type - supported types are uchar3, uchar4, float3, float4");  }
+template<typename T> inline __host__ __device__ T cast_vec( const uchar3& a )				{ static_assert(cuda_assert_false<T>::value, "invalid vector type - supported types are uchar3, uchar4, float3, float4");  }
+template<typename T> inline __host__ __device__ T cast_vec( const uchar4& a )				{ static_assert(cuda_assert_false<T>::value, "invalid vector type - supported types are uchar3, uchar4, float3, float4");  }
+template<typename T> inline __host__ __device__ T cast_vec( const float3& a )				{ static_assert(cuda_assert_false<T>::value, "invalid vector type - supported types are uchar3, uchar4, float3, float4");  }
+template<typename T> inline __host__ __device__ T cast_vec( const float4& a )				{ static_assert(cuda_assert_false<T>::value, "invalid vector type - supported types are uchar3, uchar4, float3, float4");  }
 
-template<> inline __device__ uint8_t alpha( uchar3 vec, uint8_t default_alpha )				{ return default_alpha; }
-template<> inline __device__ uint8_t alpha( uchar4 vec, uint8_t default_alpha )				{ return vec.w; }
+template<> inline __host__ __device__ uchar3 cast_vec( const uchar3& a )					{ return make_uchar3(a); }
+template<> inline __host__ __device__ uchar4 cast_vec( const uchar3& a )					{ return make_uchar4(a); }
+template<> inline __host__ __device__ float3 cast_vec( const uchar3& a )					{ return make_float3(a); }
+template<> inline __host__ __device__ float4 cast_vec( const uchar3& a )					{ return make_float4(a); }
 
-template<> inline __device__ float alpha( float3 vec, float default_alpha )				{ return default_alpha; }
-template<> inline __device__ float alpha( float4 vec, float default_alpha )				{ return vec.w; }
+template<> inline __host__ __device__ uchar3 cast_vec( const uchar4& a )					{ return make_uchar3(a); }
+template<> inline __host__ __device__ uchar4 cast_vec( const uchar4& a )					{ return make_uchar4(a); }
+template<> inline __host__ __device__ float3 cast_vec( const uchar4& a )					{ return make_float3(a); }
+template<> inline __host__ __device__ float4 cast_vec( const uchar4& a )					{ return make_float4(a); }
+
+template<> inline __host__ __device__ uchar3 cast_vec( const float3& a )					{ return make_uchar3(a); }
+template<> inline __host__ __device__ uchar4 cast_vec( const float3& a )					{ return make_uchar4(a); }
+template<> inline __host__ __device__ float3 cast_vec( const float3& a )					{ return make_float3(a); }
+template<> inline __host__ __device__ float4 cast_vec( const float3& a )					{ return make_float4(a); }
+
+template<> inline __host__ __device__ uchar3 cast_vec( const float4& a )					{ return make_uchar3(a); }
+template<> inline __host__ __device__ uchar4 cast_vec( const float4& a )					{ return make_uchar4(a); }
+template<> inline __host__ __device__ float3 cast_vec( const float4& a )					{ return make_float3(a); }
+template<> inline __host__ __device__ float4 cast_vec( const float4& a )					{ return make_float4(a); }
+
+/**
+ * @ingroup cuda
+ */
+template<typename T> inline __device__ typename cudaVectorTypeInfo<T>::Base alpha( T vec, typename cudaVectorTypeInfo<T>::Base default_alpha=255 )	{ static_assert(cuda_assert_false<T>::value, "invalid vector type - supported types are uchar3, uchar4, float3, float4");  }
+
+template<> inline __host__ __device__ uint8_t alpha( uchar3 vec, uint8_t default_alpha )		{ return default_alpha; }
+template<> inline __host__ __device__ uint8_t alpha( uchar4 vec, uint8_t default_alpha )		{ return vec.w; }
+
+template<> inline __host__ __device__ float alpha( float3 vec, float default_alpha )			{ return default_alpha; }
+template<> inline __host__ __device__ float alpha( float4 vec, float default_alpha )			{ return vec.w; }
 
 
 #endif
