251ed182845386e05d590f0a2816951700d814d6
Dustin Franklin <dustinf@nvidia.com>
2020-06-09 16:54:55 -0400

    added imageWriter

---
+++ image/imageWriter.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#include "imageWriter.h"
+#include "imageIO.h"
+#include "logging.h"
+
+#include <strings.h>
+
+
+// supported image file extensions
+const char* imageWriter::SupportedExtensions[] = { "jpg", "jpeg", "png", 
+										 "tga", "targa", "bmp", 
+										 NULL };
+
+bool imageWriter::IsSupportedExtension( const char* ext )
+{
+	if( !ext )
+		return false;
+
+	uint32_t extCount = 0;
+
+	while(true)
+	{
+		if( !SupportedExtensions[extCount] )
+			break;
+
+		if( strcasecmp(SupportedExtensions[extCount], ext) == 0 )
+			return true;
+
+		extCount++;
+	}
+
+	return false;
+}
+
+// constructor
+imageWriter::imageWriter( const videoOptions& options ) : videoOutput(options)
+{
+	mFileCount = 0;
+	mStreaming = true;
+
+	mOptions.deviceType = videoOptions::DEVICE_FILE;
+}
+
+
+// destructor
+imageWriter::~imageWriter()
+{
+
+}
+
+
+// Create
+imageWriter* imageWriter::Create( const videoOptions& options )
+{
+	return new imageWriter(options);
+}
+
+
+// Create
+imageWriter* imageWriter::Create( const char* resource, const videoOptions& options )
+{
+	videoOptions opt = options;
+	opt.resource = resource;
+	return Create(opt);
+}
+
+
+// Render
+bool imageWriter::Render( void* image, uint32_t width, uint32_t height, imageFormat format )
+{
+	const bool substreams_success = videoOutput::Render(image, width, height, format);
+
+	// format the output filename
+	if( mOptions.resource.path.find("%") != std::string::npos )
+		sprintf(mFileOut, mOptions.resource.path.c_str(), mFileCount);
+	else
+		strcpy(mFileOut, mOptions.resource.path.c_str());
+
+	// save the image
+	if( !saveImage(mFileOut, image, width, height, format) )
+	{
+		LogError(LOG_IMAGE "imageWriter -- failed to save '%s'\n", mFileOut);
+		return false;
+	}
+
+	mOptions.width  = width;
+	mOptions.height = height;
+
+	mFileCount++;
+
+	return substreams_success;
+}
+
---
+++ image/imageWriter.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
+#ifndef __IMAGE_WRITER_H_
+#define __IMAGE_WRITER_H_
+
+
+#include "videoOutput.h"
+
+
+/**
+ * @ingroup image
+ */
+class imageWriter : public videoOutput
+{
+public:
+	/**
+	 *
+	 */
+	static imageWriter* Create( const char* path, const videoOptions& options=videoOptions() );
+
+	/**
+	 *
+	 */
+	static imageWriter* Create( const videoOptions& options );
+
+	/**
+	 *
+	 */
+	virtual ~imageWriter();
+
+	/**
+	 *
+	 */
+	template<typename T> bool Render( T* image, uint32_t width, uint32_t height )		{ return Render((void**)image, width, height, imageFormatFromType<T>()); }
+	
+	/**
+	 *
+	 */
+	virtual bool Render( void* image, uint32_t width, uint32_t height, imageFormat format );
+
+	/**
+	 *
+	 */
+	static const uint32_t Type = (1 << 5);
+
+	/**
+	 *
+	 */
+	static const char* SupportedExtensions[];
+
+	/**
+	 *
+	 */
+	static bool IsSupportedExtension( const char* ext );
+
+
+protected:
+	imageWriter( const videoOptions& options );
+
+	uint32_t mFileCount;
+	char     mFileOut[1024];
+};
+
+#endif
