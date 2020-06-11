7d2a323f7ffe33976f3ce598f4ef0615f96e4f14
Dustin Franklin <dustinf@nvidia.com>
2020-06-11 16:49:40 -0400

    added video-viewer.py sample

---
+++ python/examples/video-viewer.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import jetson.utils
+
+import argparse
+import sys
+
+
+# parse command line
+def usage():
+	return "TODO additional help text"
+
+parser = argparse.ArgumentParser(description="View various types of video streams", 
+						   formatter_class=argparse.RawTextHelpFormatter, epilog=usage())
+
+parser.add_argument("input_URI", type=str, help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+
+try:
+	opt = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# create video sources & outputs
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv)
+
+# capture frames until user exits
+while output.IsStreaming():
+	image = input.Capture()
+	print(image)
+	output.Render(image)
+	output.SetStatus("Video Viewer | {:d}x{:d} | {:d} FPS".format(image.width, image.height, output.GetFrameRate()))
+	
+
+
