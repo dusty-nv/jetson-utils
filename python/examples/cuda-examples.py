1ee0494b196b488b4b95e89fa2d72366d3cf4879
Dustin Franklin <dustinf@nvidia.com>
2021-08-02 23:43:54 -0400

    updated cuda-examples.py

--- python/examples/cuda-examples.py
+++ python/examples/cuda-examples.py
@@ -29,7 +29,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Perform some example CUDA processing on an image')
 
 parser.add_argument("file_in", type=str, default="images/granny_smith_1.jpg", nargs='?', help="filename of the input image to process")
-parser.add_argument("file_out", type=str, default="cuda-example.jpg", nargs='?', help="filename of the output image to save")
+parser.add_argument("file_out", type=str, default="images/test/cuda-example.jpg", nargs='?', help="filename of the output image to save")
 
 opt = parser.parse_args()
 
@@ -72,6 +72,19 @@ input_img = jetson.utils.loadImage(opt.file_in)
 print('input image:')
 print(input_img)
 
+# copy the image (this isn't necessary, just for demonstration)
+copied_img = jetson.utils.cudaAllocMapped(width=input_img.width, height=input_img.width, format=input_img.format)
+jetson.utils.cudaMemcpy(copied_img, input_img)   # dst, src
+
+jetson.utils.cudaDeviceSynchronize()
+jetson.utils.saveImage("images/test/memcpy_0.jpg", copied_img)
+    
+# or you can use this shortcut, which will allocate the image first
+copied_img = jetson.utils.cudaMemcpy(input_img)
+
+jetson.utils.cudaDeviceSynchronize()
+jetson.utils.saveImage("images/test/memcpy_1.jpg", copied_img)
+
 # convert to grayscale - other formats are:
 #  rgb8, rgba8, rgb32f, rgba32f, gray32f
 gray_img = convert_color(input_img, "gray8")
@@ -91,9 +104,20 @@ resized_img = resize(crop_img, (0.5, 0.5))
 print('resized image:')
 print(resized_img)
 
+# convert back to color
+color_img = convert_color(resized_img, 'rgb8')
+
+print('color image')
+print(color_img)
+
+# draw some shapes
+jetson.utils.cudaDrawCircle(color_img, (50,50), 50, (0,255,127,200)) # (cx,cy), radius, color
+jetson.utils.cudaDrawRect(color_img, (200,25,350,250), (255,127,0,200)) # (left, top, right, bottom), color
+jetson.utils.cudaDrawLine(color_img, (25,150), (325,15), (255,0,200,200), 10) # (x1,y1), (x2,y2), color, thickness
+
 # save the image
 if opt.file_out is not None:
 	jetson.utils.cudaDeviceSynchronize()
-	jetson.utils.saveImage(opt.file_out, resized_img)
+	jetson.utils.saveImage(opt.file_out, color_img)
 	print("saved {:d}x{:d} test image to '{:s}'".format(crop_img.width, crop_img.height, opt.file_out))
 
