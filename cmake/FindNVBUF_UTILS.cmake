315c7d787bc6d247ee477cbd8b9f1ef91ab9b42b
Onur Bingol <61478203+onurbingol@users.noreply.github.com>
2023-06-29 17:37:45 +0300

    NVBUF_UTILS find module fixes

--- cmake/FindNVBUF_UTILS.cmake
+++ cmake/FindNVBUF_UTILS.cmake
@@ -16,11 +16,14 @@ if(NVBUF_UTILS_FOUND)
   mark_as_advanced(NVBUF_UTILS_LIBRARY)
 endif()
 
-if (NVBUF_UTILS_FOUND AND NOT TARGET JetsonUtils::NVBUF_UTILS)
-  add_library(JetsonUtils::NVBUF_UTILSIMPORTED)
+if(NVBUF_UTILS_FOUND AND NOT TARGET JetsonUtils::NVBUF_UTILS)
+  add_library(JetsonUtils::NVBUF_UTILS IMPORTED)
   set_property(TARGET JetsonUtils::NVBUF_UTILS PROPERTY IMPORTED_LOCATION ${NVBUF_UTILS_LIBRARY})
   target_include_directories(JetsonUtils::NVBUF_UTILS INTERFACE ${NVBUF_UTILS_INCLUDE_DIR})
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NVBUF_UTILS REQUIRED_VARS NVBUF_UTILS_LIBRARY NVBUF_UTILS_INCLUDE_DIR)
+find_package_handle_standard_args(NVBUF_UTILS
+  REQUIRED_VARS
+    NVBUF_UTILS_LIBRARY NVBUF_UTILS_INCLUDE_DIR
+)
