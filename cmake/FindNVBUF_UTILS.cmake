478ddf3bc6c2faf5b1b54530eaf42daf486c770a
Onur Bingol <61478203+onurbingol@users.noreply.github.com>
2023-06-29 12:37:09 +0300

    add FindNVBUF_UTILS module

---
+++ cmake/FindNVBUF_UTILS.cmake
@@ -0,0 +1,26 @@
+# FindNVBUF_UTILS.cmake
+
+find_library(NVBUF_UTILS_LIBRARY
+  NAMES nvbuf_utils
+  PATHS /usr/lib/aarch64-linux-gnu/tegra
+)
+
+find_path(
+  NVBUF_UTILS_INCLUDE_DIR
+  NAMES include/nvbuf_utils.h
+  PATHS /usr/src/jetson_multimedia_api
+)
+
+if(NVBUF_UTILS_FOUND)
+  mark_as_advanced(NVBUF_UTILS_INCLUDE_DIR)
+  mark_as_advanced(NVBUF_UTILS_LIBRARY)
+endif()
+
+if (NVBUF_UTILS_FOUND AND NOT TARGET JetsonUtils::NVBUF_UTILS)
+  add_library(JetsonUtils::NVBUF_UTILSIMPORTED)
+  set_property(TARGET JetsonUtils::NVBUF_UTILS PROPERTY IMPORTED_LOCATION ${NVBUF_UTILS_LIBRARY})
+  target_include_directories(JetsonUtils::NVBUF_UTILS INTERFACE ${NVBUF_UTILS_INCLUDE_DIR})
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(NVBUF_UTILS REQUIRED_VARS NVBUF_UTILS_LIBRARY NVBUF_UTILS_INCLUDE_DIR)
