d358066385073345ccd242eb4d77716a356af138
Onur Bingol <onur.bingol@rios.ai>
2023-08-04 18:22:53 -0700

    refactor CMakeLists for the python bindings

--- python/python/jetson/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-
-#print("Jetson.Utils.__init__.py")
-
-from jetson_utils_python import *
-
-VERSION = '1.0.0'
-
-print("warning:  importing jetson.utils is deprecated.  please 'import jetson_utils' instead.")
